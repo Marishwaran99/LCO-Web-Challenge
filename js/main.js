e58e2e3915aa0bc4f5ace531cc2e2eb7baca1606
marishwaran <marishwaran.k@somaiya.edu>
2020-03-21 21:18:54 +0530

    latest

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$('.team__wrapper').slick({
-		slidesToShow   : 3,
-		slidesToScroll : 1,
-		asNavFor       : '.team__wrapper',
-		dots           : true,
-		arrows         : false
+	$('.carousel').slick({
+		slidesToShow : 3,
+		dots         : true,
+		centerMode   : false,
+		autoplay     : false,
+		arrows       : true
 	});
 });
