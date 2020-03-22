d682e2dd2b57d112e11fc5f525c43a5a6609cf4e
marishwaran <marishwaran.k@somaiya.edu>
2020-03-22 21:44:20 +0530

    latest

--- js/main.js
+++ js/main.js
@@ -1,49 +1,31 @@
-$(document).ready(function() {
-	$('.nav__btn').click(function(e) {
-		$('.nav__links').toggleClass('active');
-		e.stopPropagation();
-	});
-	$(document).click(function() {
-		if ($('.nav__links').hasClass('active')) {
-			$('.nav__links').removeClass('active');
-		}
-	});
-	$('.nav__link').click(function() {
-		$('.nav__link').removeClass('active');
-		$(this).addClass('active');
-		$('.nav__links').removeClass('active');
-	});
-	$('.carousel').slick({
-		slidesToShow : 3,
-		dots         : true,
-		centerMode   : false,
-		autoplay     : false,
-		arrows       : true,
-		loop         : false,
-		responsive   : [
-			{
-				breakpoint : 1024,
-				settings   : {
-					slidesToShow   : 3,
-					slidesToScroll : 3,
-					infinite       : true,
-					dots           : true
-				}
-			},
-			{
-				breakpoint : 600,
-				settings   : {
-					slidesToShow   : 2,
-					slidesToScroll : 2
-				}
-			},
-			{
-				breakpoint : 480,
-				settings   : {
-					slidesToShow   : 1,
-					slidesToScroll : 1
-				}
-			}
-		]
-	});
+var nav_btn = document.querySelector('.nav__btn');
+var nav_links = document.querySelector('.nav__links');
+var nav_link_list = document.querySelectorAll('.nav__link');
+var main_body = document.querySelector('main');
+document.addEventListener(
+	'touchstart',
+	function(e) {
+		e.preventDefault();
+	},
+	{ passive: true }
+);
+nav_btn.addEventListener('click', function() {
+	console.log('hello');
+	nav_links.classList.toggle('active');
 });
+main_body.addEventListener('click', function() {
+	console.log('hello');
+	nav_links.classList.remove('active');
+});
+for (let i = 0; i < nav_link_list.length; i++) {
+	nav_link_list[i].addEventListener('click', function() {
+		removeActive();
+		nav_link_list[i].classList.add('active');
+	});
+}
+
+function removeActive() {
+	for (let i = 0; i < nav_link_list.length; i++) {
+		nav_link_list[i].classList.remove('active');
+	}
+}
